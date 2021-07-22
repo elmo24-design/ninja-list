6d4052751bcf0223a5d83b687f8ba8e488f522bf
ElmoMahupil <eoh@misout.com>
2021-07-22 16:38:20 +0800

    2nd commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
+  reactStrictMode: true
 }
